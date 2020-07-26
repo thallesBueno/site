b9a79e3f428b06784953028d1d8894e9b233dc4e
thallesBueno <thalles.bueno@softdesign.com.br>
2020-07-26 00:07:08 -0300

    config styled components, global styled and defaul theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './styles/themes/defaultTheme';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div>
-      Thalles Bueno
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>
+        Thalles Bueno
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/themes/defaultTheme.js
@@ -0,0 +1,11 @@
+const defaultTheme = {
+  name: 'defaultTheme',
+  colors: {
+    mainColor: '#fee440',
+    mainColorContrast: '#fff',
+    textColor: '#1A1A1A',
+    textColorContrast: '#fff',
+  },
+};
+
+export default defaultTheme;
