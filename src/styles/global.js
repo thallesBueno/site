268f4d591548cdca13afc67e9e0afa654734f825
thallesBueno <thalles.bueno@softdesign.com.br>
2020-07-26 00:40:30 -0300

    config font montserrat

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
+    font-family: 'Montserrat', sans-serif;
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
